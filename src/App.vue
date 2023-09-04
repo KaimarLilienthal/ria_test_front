8d929b05b159c7dfa7cf885546d9c85d5158f1ee
Kaimar <kaimarlilienthal@gmail.com>
2023-09-04 17:18:38 +0300

    WP1

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,75 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div style="padding-top: 30px; padding-bottom: 30px;">
+    <header class="navbar" style="background-color: white; padding: unset">
+      <div class="container">
+
+        <div class="col-auto justify-content-start">
+          <div style="padding-right: 80px;">
+            <a class="navbar-brand">
+              <img src="@/assets/logo.svg" alt="Logo">
+            </a>
+          </div>
+        </div>
+
+        <div class="col-1-auto">
+          <router-link to="/" class="nav-link" style="color: #080880; font-size: smaller; padding: 18px 0.5rem; font-weight: bold;">AVALEHT</router-link>
+        </div>
+
+        <div class="col-2-auto">
+          <router-link to="/event/new" class="nav-link" style="color: #080880; font-size: smaller; padding: 18px 0.5rem; font-weight: bold;">ÜRITUSE LISAMINE</router-link>
+        </div>
+
+
+
+        <div class="flex-grow-1 d-flex justify-content-end">
+          <a>
+            <img src="@/assets/symbol.svg" alt="Symbol" width="50" height="50">
+          </a>
+        </div>
+
+      </div>
+    </header>
+    <router-view/>
+
+    <footer class="footer" style="background-color: #282727; padding: 20px;">
+      <div class="container text-lg-start">
+        <div class="row">
+          <div class="col" style="color: white">
+            <h4>Fusce</h4>
+            <h6>Econsectetur</h6>
+            <h6>Ksollicitudin</h6>
+            <h6>omvulputate</h6>
+            <h6>Nunc fringilla tellu</h6>
+          </div>
+          <div class="col" style="color: white">
+            <h4>Curabitur</h4>
+            <h6>Emauris</h6>
+            <h6>Kfringilla</h6>
+            <h6>Oin magna sem</h6>
+            <h6>Kelementum</h6>
+          </div>
+          <div class="col" style="color: white">
+            <h4>Kontakt</h4>
+            <h6>Peakontor: Tallinnas</h6>
+            <h6>Väike-Ameerika 1, 11415 Tallinn</h6>
+            <h6>Telefon: 605 4450</h6>
+            <h6>Faks:605 3186</h6>
+          </div>
+          <div class="col" style="color: white">
+            <h4 class="empty-space">.</h4>
+            <h6>Harukontor: Võrus</h6>
+            <h6>Oja tn 7 (külastusaadress)</h6>
+            <h6>Telefon: 605 3330</h6>
+            <h6>Faks: 605 3155</h6>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+<script>
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,156 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="padding-top: 30px;" class="container">
+    <div class="row g-0">
+      <div class="col-6 d-flex align-items-center " style="background-color: darkblue">
+        <div class="form-floating mb-3" height="313" width="600">
+          <div style="color: white; display: flex; align-items: baseline;">
+            <span class="indent" style="font-size: 1.25rem;">Sed nec elit vestibulum, <span class="bold-text">tincidunt orci</span> et, sagittis</span>
+          </div>
+          <div style="color: white; display: flex; align-items: baseline;">
+            <span class="indent" style="font-size: 1.25rem;">ex. Vestibulum rutrum <span class="bold-text">neque suscipit</span> ante</span>
+          </div>
+          <div style="color: white; display: flex; align-items: baseline;">
+            <span class="indent" style="font-size: 1.25rem;">mattis maximus. Nulla non sapien <span class="bold-text">viverra,</span></span>
+          </div>
+          <div style="color: white; display: flex; align-items: baseline;">
+            <span class="indent" style="font-size: 1.25rem;"><span class="bold-text">labortis lorem non</span>, accumsan metus.</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-6">
+
+        <img src="../assets/pilt.jpg" height="319" width="558"/>
+      </div>
+    </div>
+  </div>
+  <div style="padding-top: 30px;" class="container">
+    <div class="row" style="padding-left: 13px;">
+      <div class="col-6 d-flex align-items-center justify-content-center"
+           style="background-color: darkblue; width:calc(50% - 5px); height: 10vh">
+        <div>
+          <div style="color: white;">
+            <span class="indent" style="font-size: 1.25rem;">Tulevased üritused</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-6 d-flex align-items-center  justify-content-center">
+        <div style="background-color: darkblue; height: 100%; width:calc(100% + 5px);">
+          <div class="d-flex align-items-center justify-content-center" style="color: white; height: 100%;">
+            <span class="indent" style="font-size: 1.25rem;">Toimunud üritused</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container" style="padding-bottom: 30px">
+    <div class="row" style="padding-left: 13px;">
+      <div class="col-6"
+           style="background-color: white; width:calc(50% - 5px)">
+        <div style="width: 545px; margin: 0 auto; margin-left: 0 !important;">
+          <table class="table table-borderless" style="color: darkblue; padding-left: 13px;">
+            <thead>
+            <tr>
+              <th scope="col"></th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+              <th scope="col"></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Nunc lobortis metus eu massa viverra ultri iplacerat nibh</td>
+              <td>03-03-2016</td>
+              <td>OSAVÕTJAD</td>
+              <td>X</td>
+            </tr>
+            <tr>
+              <th scope="row">1</th>
+              <td>Nunc lobortis metus eu massa viverra ultri iplacerat nibh</td>
+              <td>03-03-2016</td>
+              <td>OSAVÕTJAD</td>
+              <td>X</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="d-flex align-items-center justify-content-between">
+          <button type="button" @click="toAddNewEvent" class="btn btn-sm">LISA ÜRITUS</button>
+        </div>
+      </div>
+      <div class="col-6 d-flex align-items-center  justify-content-center">
+        <div style="background-color: white; height: 100%; width:calc(100% + 5px);">
+          <div class="d-flex align-items-center justify-content-center" style="color: darkblue; height: 100%;">
+            <table class="table table-borderless" style="color: darkblue; padding-left: 13px;">
+              <thead>
+              <tr>
+                <th scope="col"></th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Nunc lobortis metus eu massa viverra ultri iplacerat nibh</td>
+                <td>03-03-2016</td>
+                <td>OSAVÕTJAD</td>
+                <td>X</td>
+              </tr>
+              <tr>
+                <th scope="row">1</th>
+                <td>Nunc lobortis metus eu massa viverra ultri iplacerat nibh</td>
+                <td>03-03-2016</td>
+                <td>OSAVÕTJAD</td>
+                <td>X</td>
+              </tr>
+              <tr>
+              <th scope="row"></th>
+              </tr>
+              <tr>
+              <th scope="row"></th>
+              </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import router from "@/router";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: 'homeRoute',
+  methods: {
+    toAddNewEvent(){
+      router.push({name: 'addNewEventRoute'})
+    }
   }
 }
 </script>
+
+<style>
+.text-left {
+  display: inline-block;
+  text-align: left;
+}
+
+.cap-left {
+  display: inline-block;
+  width: 20px; /* Adjust the width to control the size of the "cap" */
+}
+
+.indent {
+  margin-left: 40px; /* Adjust the indentation amount as needed */
+}
+
+.bold-text {
+  font-weight: bold;
+}
+</style>
